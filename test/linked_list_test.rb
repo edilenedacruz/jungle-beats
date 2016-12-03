f5c3623974e75d6db0dd5ba81637eebd722404e6
Edilene <edilene_cruz@yahoo.com.br>
2016-12-03 10:00:22 -0700

    completed iteration 0

---
+++ test/linked_list_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/linked_list'
+
+class LinkedListTest < Minitest::test
+end
---
+++ test/node_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require "./lib/node"
+
+class NodeTest < Minitest::Test
+
+  def test_it_can_access_data
+    node = Node.new("plop")
+
+    assert_equal "plop", node.data
+  end
+
+  def test_next_node_is_nil 
+    node = Node.new("plop")
+
+    assert_equal nil, node.next_node
+  end
+end
